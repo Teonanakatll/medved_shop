73802d124f2800d3ba7870101f0dd263ef233ac6
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-04-16 23:21:06 +0300

    #1.10.1____Запускаем static2

--- medved/medved/settings.py
+++ medved/medved/settings.py
@@ -121,12 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_prod')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', 'static_dev')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
